b0edbfc92de0ad88177ad0adc439d91f200641df
huangjoseph45 <huangjoseph45@gmail.com>
2025-03-12 13:07:48 -0400

    s

--- src/components/header-components/profile-button.jsx
+++ src/components/header-components/profile-button.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import isLoggedIn from "../utilities/isLoggedIn";
+import { motion } from "motion/react";
 
 const ProfileButton = ({ showLogin, setShowLogin, setShowSidebar }) => {
   const loggedIn = isLoggedIn();
@@ -17,7 +18,11 @@ const ProfileButton = ({ showLogin, setShowLogin, setShowSidebar }) => {
 
   return (
     <>
-      <a
+      <motion.a
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0 }}
+        transition={{ type: "spring", stiffness: 400, damping: 20, mass: 0.5 }}
         className="relative aspect-square rounded-full cursor-pointer hover:bg-slate-500 hover:bg-opacity-25 p-2 hover:text-bgTertiary"
         href={loggedIn ? "/profile" : undefined} // Properly conditionally set href
         onClick={clickedFunc}
@@ -40,7 +45,7 @@ const ProfileButton = ({ showLogin, setShowLogin, setShowSidebar }) => {
             d="M7.5 42v-6a7.5 7.5 0 017.5-7.5h18a7.5 7.5 0 017.5 7.5v6m-9-27a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z"
           ></path>
         </svg>
-      </a>
+      </motion.a>
     </>
   );
 };
--- src/components/header-components/search-button.jsx
+++ src/components/header-components/search-button.jsx
@@ -1,7 +1,13 @@
+import { motion } from "motion/react";
+
 const SearchButton = ({ setSearch }) => {
   return (
     <>
-      <div
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0 }}
+        transition={{ type: "spring", stiffness: 400, damping: 20, mass: 0.5 }}
         title="Search"
         onClick={() => setSearch(true)}
         className="cursor-pointer aspect-square  hover:bg-slate-500 hover:bg-opacity-25 p-2 rounded-full hover:text-bgTertiary"
@@ -22,7 +28,7 @@ const SearchButton = ({ setSearch }) => {
             d="M13.962 16.296a6.716 6.716 0 01-3.462.954 6.728 6.728 0 01-4.773-1.977A6.728 6.728 0 013.75 10.5c0-1.864.755-3.551 1.977-4.773A6.728 6.728 0 0110.5 3.75c1.864 0 3.551.755 4.773 1.977A6.728 6.728 0 0117.25 10.5a6.726 6.726 0 01-.921 3.407c-.517.882-.434 1.988.289 2.711l3.853 3.853"
           ></path>
         </svg>
-      </div>
+      </motion.div>
     </>
   );
 };
