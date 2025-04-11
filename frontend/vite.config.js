d1b7be9090c1b181692176b0e0ca0fadd525ebe1
huangjoseph45 <huangjoseph45@gmail.com>
2025-04-11 13:27:10 -0400

    i love fixing in prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,20 +9,18 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    "/api": {
-      target: "https://ecommerce-main-s1rb.onrender.com",
-      changeOrigin: true,
-      secure: true,
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://ecommerce-main-s1rb.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
     },
   },
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-  },
-  // Optional: Accessing process.env variables manually (if needed)
-  define: {
-    "process.env": process.env,
   },
 });
